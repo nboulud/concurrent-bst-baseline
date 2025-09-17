6a821d023161c76fbad0514b9621b3f6baf7bb79
nboulud <noe.boulud@epfl.ch>
2025-09-17 16:23:17 +0200

    augmented tree version 1 test pass

--- Code/src/main/java/bst/MyBST.java
+++ Code/src/main/java/bst/MyBST.java
@@ -1,6 +1,8 @@
 package bst;
 
 import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
+import java.util.ArrayDeque;
+import java.util.Objects;
 
 public class MyBST<K extends Comparable<? super K>, V> {
     //--------------------------------------------------------------------------------
@@ -8,31 +10,57 @@ public class MyBST<K extends Comparable<? super K>, V> {
     //--------------------------------------------------------------------------------
     protected static abstract class Node<E extends Comparable<? super E>, V> {
         final E key;
-
+        volatile InternalNode<E,V> parent;
         Node(final E key) {
             this.key = key;
         }
     }
 
+    protected static final class Version<E extends Comparable<? super E>> {
+        final E key;
+        final Version<E> left;
+        final Version<E> right;
+        final int nbChild;    // sum of keys if K is Integer; else 0
+
+        Version(E key, Version<E> left, Version<E> right, int nbChild) {
+            this.key = key;
+            this.left = left;
+            this.right = right;
+            this.nbChild = nbChild;
+        }
+    }
+
     protected final static class LeafNode<E extends Comparable<? super E>, V> extends Node<E,V> {
         final V value;
+        volatile Version<E> version;
 
         LeafNode(final E key, final V value) {
             super(key);
             this.value = value;
+            int s = (key == null) ? 0 : 1;
+            this.version = new Version<>(key, null, null, s);
         }
     }
 
     protected final static class InternalNode<E extends Comparable<? super E>, V> extends Node<E,V> {
         volatile Node<E,V> left;
         volatile Node<E,V> right;
+        volatile Node<E,V> parent;
         volatile Info<E,V> info;
+        volatile Version<E> version;
+
 
         InternalNode(final E key, final LeafNode<E,V> left, final LeafNode<E,V> right) {
             super(key);
             this.left = left;
             this.right = right;
             this.info = null;
+            if (left  != null)  left.parent  = this;
+            if (right != null)  right.parent = this;
+            Version<E> vL = left.version;
+            Version<E> vR = right.version;
+            final int nb = (vL != null ? vL.nbChild : 0) + (vR != null ? vR.nbChild : 0);
+            this.version = new Version<>(key, vL, vR, nb);
         }
     }
 
@@ -81,6 +109,8 @@ public class MyBST<K extends Comparable<? super K>, V> {
     private static final AtomicReferenceFieldUpdater<InternalNode, Node> leftUpdater = AtomicReferenceFieldUpdater.newUpdater(InternalNode.class, Node.class, "left");
     private static final AtomicReferenceFieldUpdater<InternalNode, Node> rightUpdater = AtomicReferenceFieldUpdater.newUpdater(InternalNode.class, Node.class, "right");
     private static final AtomicReferenceFieldUpdater<InternalNode, Info> infoUpdater = AtomicReferenceFieldUpdater.newUpdater(InternalNode.class, Info.class, "info");
+    private static final AtomicReferenceFieldUpdater<InternalNode, Version> versionUpdater = AtomicReferenceFieldUpdater.newUpdater(InternalNode.class, Version.class, "version");
+
 
     final InternalNode<K,V> root;
 
@@ -146,6 +176,7 @@ public class MyBST<K extends Comparable<? super K>, V> {
             LeafNode<K,V> foundLeaf = (LeafNode<K,V>)l;
 
             if (key.equals(foundLeaf.key)) {
+                propagate(p);
                 return foundLeaf.value; // key already in the tree, no duplicate allowed
             } else if (!(pinfo == null || pinfo.getClass() == Clean.class)) {
                 help(pinfo);
@@ -156,11 +187,15 @@ public class MyBST<K extends Comparable<? super K>, V> {
                 else
                     newInternal = new InternalNode<K,V>(key, newSibling, newNode);
 
+                newInternal.parent = p;
+                newSibling.parent = newInternal; newNode.parent = newInternal;
+
                 final IInfo<K,V> newPInfo = new IInfo<K,V>(foundLeaf, p, newInternal);
 
                 // try to IFlag parent
                 if (infoUpdater.compareAndSet(p, pinfo, newPInfo)) {
                     helpInsert(newPInfo);
+                    propagate(p);
                     return null;
                 } else {
                     // if fails, help the current operation
@@ -211,6 +246,7 @@ public class MyBST<K extends Comparable<? super K>, V> {
                 if (key.equals(foundLeaf.key)) {
                     // key already in the tree, try to replace the old node with new node
                     newPInfo = new IInfo<K, V>(foundLeaf, p, newNode);
+                    propagate(p);
                     result = foundLeaf.value;
                 } else {
                     // key is not in the tree, try to replace a leaf with a small subtree
@@ -221,7 +257,8 @@ public class MyBST<K extends Comparable<? super K>, V> {
                     } else {
                         newInternal = new InternalNode<K, V>(key, newSibling, newNode);
                     }
-
+                    newInternal.parent = p;
+                    newSibling.parent = newInternal; newNode.parent = newInternal;
                     newPInfo = new IInfo<K, V>(foundLeaf, p, newInternal);
                     result = null;
                 }
@@ -229,6 +266,7 @@ public class MyBST<K extends Comparable<? super K>, V> {
                 // try to IFlag parent
                 if (infoUpdater.compareAndSet(p, pinfo, newPInfo)) {
                     helpInsert(newPInfo);
+                    propagate(p);
                     return result;
                 } else {
                     // if fails, help the current operation
@@ -262,7 +300,7 @@ public class MyBST<K extends Comparable<? super K>, V> {
             l = p.left;
             while (l.getClass() == InternalNode.class) {
                 gp = p;
-                p = (InternalNode<K,V>)l;
+                p = (InternalNode<K, V>) l;
                 l = (p.key == null || key.compareTo(p.key) < 0) ? p.left : p.right;
             }
             // note: gp can be null here, because clearly the root.left.left == null
@@ -276,8 +314,9 @@ public class MyBST<K extends Comparable<? super K>, V> {
             }
             /** END SEARCH **/
 
-            if (!key.equals(l.key)) return null;
-            if (!(gpinfo == null || gpinfo.getClass() == Clean.class)) {
+            if (!key.equals(l.key)) {
+                propagate(p); return null;
+            }else if (!(gpinfo == null || gpinfo.getClass() == Clean.class)) {
                 help(gpinfo);
             } else if (!(pinfo == null || pinfo.getClass() == Clean.class)) {
                 help(pinfo);
@@ -303,7 +342,17 @@ public class MyBST<K extends Comparable<? super K>, V> {
 //--------------------------------------------------------------------------------
 
     private void helpInsert(final IInfo<K,V> info){
-        (info.p.left == info.l ? leftUpdater : rightUpdater).compareAndSet(info.p, info.l, info.lReplacingNode);
+        boolean onLeft = (info.p.left == info.l);
+        boolean spliced = onLeft
+                ? leftUpdater.compareAndSet(info.p, info.l, info.lReplacingNode)
+                : rightUpdater.compareAndSet(info.p, info.l, info.lReplacingNode);
+
+        if (spliced) {
+            // Fix parent of the new child
+            info.lReplacingNode.parent = info.p;
+            // NEW: ensure snapshot is refreshed even if a helper did the splice
+            propagate(info.p);
+        }
         infoUpdater.compareAndSet(info.p, info, new Clean());
     }
 
@@ -331,8 +380,58 @@ public class MyBST<K extends Comparable<? super K>, V> {
 
     private void helpMarked(final DInfo<K,V> info) {
         final Node<K,V> other = (info.p.right == info.l) ? info.p.left : info.p.right;
-        (info.gp.left == info.p ? leftUpdater : rightUpdater).compareAndSet(info.gp, info.p, other);
-        infoUpdater.compareAndSet(info.gp, info, new Clean());
+        boolean pIsLeft = (info.gp.left == info.p);
+        boolean swung = pIsLeft
+                ? leftUpdater.compareAndSet(info.gp, info.p, other)
+                : rightUpdater.compareAndSet(info.gp, info.p, other);
+
+        if (swung) {
+            // Fix parent of the moved-up child
+            other.parent = info.gp;
+            // Keep snapshot fresh when a helper completes the swing
+            propagate(info.gp);
+        }
+        infoUpdater.compareAndSet(info.gp, info, new Clean<>());
+    }
+
+    private static <E extends Comparable<? super E>, T> Version<E> childVersion(Node<E,T> n) {
+        if (n instanceof InternalNode) return ((InternalNode<E,T>) n).version;
+        return ((LeafNode<E,T>) n).version;
+    }
+
+    private static <E extends Comparable<? super E>, T> boolean refresh(InternalNode<E,T> x) {
+        // snapshot old
+        final Version<E> old = x.version;
+
+        // Read left consistently: (ptr, then version derived from same ptr, recheck ptr)
+        Node<E,T> xL; Version<E> vL;
+        do {
+            xL = x.left;
+            vL = (xL instanceof InternalNode) ? ((InternalNode<E,T>) xL).version : ((LeafNode<E,T>) xL).version;
+        } while (x.left != xL);
+
+        // Read right consistently
+        Node<E,T> xR; Version<E> vR;
+        do {
+            xR = x.right;
+            vR = (xR instanceof InternalNode) ? ((InternalNode<E,T>) xR).version : ((LeafNode<E,T>) xR).version;
+        } while (x.right != xR);
+        int nb = vL.nbChild + vR.nbChild;
+        Version<E> newer = new Version<>(x.key, vL, vR, nb);
+        return versionUpdater.compareAndSet(x, old, newer);
+    }
+
+    private static <E extends Comparable<? super E>, T> void propagate(Node<E,T> start) {
+        Node<E,T> x = start;
+        int tries = 0;
+        while (x != null) {
+            if (x instanceof InternalNode) {
+                if (!refresh((InternalNode<E,T>) x)) {
+                    refresh((InternalNode<E,T>) x);
+                }
+            }
+            x = x.parent;
+        }
     }
 
     /**
@@ -352,5 +451,41 @@ public class MyBST<K extends Comparable<? super K>, V> {
         else
             sum += getSumOfKeys(((InternalNode<K,V>)node).left) + getSumOfKeys(((InternalNode<K,V>)node).right);
         return sum;
+    }
+
+    public int sizeStructural() {
+        return sizeStructural(root);
+    }
+    private int sizeStructural(Node<K,V> n) {
+        if (n instanceof LeafNode) return (((LeafNode<K,V>) n).key != null) ? 1 : 0;
+        InternalNode<K,V> i = (InternalNode<K,V>) n;
+        return sizeStructural(i.left) + sizeStructural(i.right);
+    }
+
+    public int sizeSnapshot() {
+        Version<K> v = root.version;
+        return (v != null) ? v.nbChild : 0;
+    }
+
+    public boolean containsKeySnapshot(K key) {
+        Objects.requireNonNull(key, "key");
+        Version<K> v = root.version;
+        if (v == null) return false;
+        while (v.left != null) {
+            if (v.key == null || key.compareTo(v.key) < 0) v = v.left;
+            else v = v.right;
+        }
+        return (v.key != null) && (key.compareTo(v.key) == 0);
+    }
+
+    public K selectKth(int j) {
+        Version<K> v = root.version;
+        if (v == null || j <= 0 || j > v.nbChild) return null;
+        while (v.left != null) {
+            int leftSize = (v.left != null) ? v.left.nbChild : 0;
+            if (j <= leftSize) v = v.left;
+            else { j -= leftSize; v = v.right; }
+        }
+        return v.key;
     }
 }
